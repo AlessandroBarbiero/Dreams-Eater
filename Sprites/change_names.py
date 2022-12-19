15b2b692b3e6eaf2666e716d8fe2a261e22d873a
aleba <aleba@itu.dk>
2022-12-19 23:05:49 +0100

    Add new assets

--- Sprites/change_names.py
+++ Sprites/change_names.py
@@ -1,15 +1,31 @@
 import os
 
+# Look in all subdirectories of rootDir, there will be the Characters, 
+# for each one of the subfolders of that folders change the names with increasing numbers -> they will be the animations
+# example of folder organization ./StillNoAtlas/BigTroll/Attack1/0.png
+def changeAllFileNamesInSubSubFolders(rootDir = './StillNoAtlas'):
+    count = 0
 
-folder = "Characters/Robot"
-subfolder = "Walk"
+    for dirName, subdirList, _ in os.walk(rootDir):
+        count+=1
+        if count == 1:
+            continue
+        i = 0
+        for subdir in subdirList:
+            print("-- subdir \t" +subdir)
+            subdirName = os.path.join(dirName, subdir)
+            files = os.listdir(subdirName)
+            for i in range(len(files)):
+                print(files[i])
+                old_fname = os.path.join(subdirName, files[i])
+                new_fname = os.path.join(subdirName, str(i)+".png")
+                os.rename(old_fname, new_fname)
 
-os.chdir(f"{folder}//{subfolder}")
 
-filenames = [x[2] for x in os.walk(".")][0]
+def changeNames(folder = "Characters/Robot", subfolder = "Walk"):
+    os.chdir(f"{folder}//{subfolder}")
 
-for i in range(len(filenames)):
-    os.rename(filenames[i], str(i)+".png")
+    filenames = [x[2] for x in os.walk(".")][0]
 
-
-
+    for i in range(len(filenames)):
+        os.rename(filenames[i], str(i)+".png")
