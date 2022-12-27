a87faaee91f17505d87c3cacb2a4526a19f1035c
Lucas <lucn@itu.dk>
2022-12-27 16:28:47 +0100

    Minimap complete

--- Header/Components/LevelGuiComponent.hpp
+++ Header/Components/LevelGuiComponent.hpp
@@ -33,6 +33,7 @@ private:
     ImVec2 menuSize = { 250, 250 };
     ImVec2 buttonSize = { 200, 100 };
 
+    ImU32 borderColor = IM_COL32(255, 255, 255, 255);
     ImU32 roomColor = IM_COL32(0, 0, 0, 255);
     ImU32 doorColor = IM_COL32(255, 0, 0, 255);
 
--- Src/Components/LevelGuiComponent.cpp
+++ Src/Components/LevelGuiComponent.cpp
@@ -18,7 +18,8 @@ void LevelGuiComponent::setPlayer(std::shared_ptr<GameObject> player) {
 
 void LevelGuiComponent::drawRoom(std::shared_ptr<RoomSettings> roomSettings, ImVec2 topLeft, ImVec2 bottomRight, ImVec2 size){
 
-    ImGui::GetWindowDrawList()->AddRectFilled(topLeft, bottomRight, roomColor);
+    ImGui::GetWindowDrawList()->AddRectFilled(topLeft, bottomRight, borderColor);
+    ImGui::GetWindowDrawList()->AddRectFilled({ topLeft.x + 1, topLeft.y + 1 }, { bottomRight.x - 1, bottomRight.y - 1 }, roomColor);
     float radius = 2.0f;
 
     auto halfSize = ImVec2{ size.x / 2.0f, size.y / 2.0f };
@@ -120,65 +121,62 @@ void LevelGuiComponent::onGui() {
     //if (level->roomEntered[i]) {}
 
     //show current room
-    ImVec2 topLeft = { menuSize.x / 2.0f - size.x / 2.0f + menuPosition.x, menuSize.y / 2.0f - size.y / 2.0f + menuPosition.y };
-    ImVec2 bottomRight = { menuSize.x / 2.0f + size.x / 2.0f + menuPosition.x , menuSize.y / 2.0f + size.y / 2.0f + menuPosition.y };
-    drawRoom(currentRoom, topLeft, bottomRight, size);
+    ImVec2 centerTopLeft = { menuSize.x / 2.0f - size.x / 2.0f + menuPosition.x, menuSize.y / 2.0f - size.y / 2.0f + menuPosition.y };
+    ImVec2 centerBottomRight = { menuSize.x / 2.0f + size.x / 2.0f + menuPosition.x , menuSize.y / 2.0f + size.y / 2.0f + menuPosition.y };
+    roomColor = IM_COL32(0, 0, 128, 255);
+    drawRoom(currentRoom, centerTopLeft, centerBottomRight, size);
 
     int i = -1;
     //explore doors
 
     auto border = 3.0f;
 
-    for (auto& const door : currentRoom->doors) {
-        auto newRoomSettings = level->roomSettings[door.destinationRoom];
-        if (!door.locked && level->roomEntered[door.destinationRoom]) {
-            switch (door.position) {
-            case Top:
-            {
-                ImVec2 newRoomSize = { newRoomSettings->size.x * scale, newRoomSettings->size.y * scale };
-                auto doorPos = ImVec2{ topLeft.x + size.x / 2.0f, topLeft.y };
-                switch (door.destinationPosition) {
-                case Bottom:
-                    ImVec2 bottomRight = { doorPos.x + newRoomSize.x / 2.0f , doorPos.y - border };
-                    ImVec2 topLeft = { doorPos.x - newRoomSize.x / 2.0f ,  bottomRight.y - border - newRoomSize.y};
-                    drawRoom(newRoomSettings, topLeft, bottomRight, newRoomSize);
-                }
 
-                
-
-                
-                break;
-            }
-            case TopLeft:
-                break;
-            case TopRight:
-                break;
-            case Bottom:
-                break;
-            case BottomLeft:
-                break;
-            case BottomRight:
-                break;
-            case Left:
-                break;
-            case LeftTop:
-                break;
-            case LeftBottom:
-                break;
-            case Right:
-                break;
-            case RightTop:
-                break;
-            case RightBottom:
-                break;
-            }
-        }
+    glm::vec2 currPos1 = currentRoom->positions[0];
+    glm::vec2 currPos2 = currentRoom->positions[0];
+    switch (currentRoom->roomSize)
+    {
+    case Medium:
+        currPos2 = currentRoom->positions[0];
+        break;
+    case Wide:
+    case Long:
+        currPos2 = currentRoom->positions[1];
+        break;
+    case Large:
+        currPos2 = currentRoom->positions[3];
+        break;
     }
-    
-        
-        
-    
-    
+    roomColor = IM_COL32(0, 0, 0, 255);
+    for (auto room : level->roomSettings )
+    {
+        if (room == currentRoom || !level->roomEntered[room->id]) {
+            continue;
+        }
+        glm::vec2 pos1 = room->positions[0];
+        glm::vec2 pos2 = room->positions[0];
+        switch (room->roomSize)
+        {
+        case Medium:
+            pos2 = room->positions[0];
+            break;
+        case Wide:
+        case Long:
+            pos2 = room->positions[1];
+            break;
+        case Large:
+            pos2 = room->positions[3];
+            break;
+        }
+        roomSize = room->size;
+        ImVec2 drawSize = { roomSize.x * scale, roomSize.y * scale };
+        auto squareSize = glm::vec2(7 * scale);
+        auto offset1 = (pos1 - currPos1) * squareSize;
+        auto offset2 = (pos2 - currPos2) * squareSize;
+        ImVec2 topLeft = { centerTopLeft.x + offset1.x, centerTopLeft.y - offset1.y };
+        ImVec2 bottomRight = { centerBottomRight.x + offset2.x, centerBottomRight.y - offset2.y };
+        drawRoom(room, topLeft, bottomRight, drawSize);
+    }
 
     ImGui::End();
     ImGui::PopStyleVar();
